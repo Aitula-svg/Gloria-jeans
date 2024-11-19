b751bb399d76e6d78202a0b1254f8c9905c51bfa
Munaraaz <munmk@bk.ru>
2024-11-19 23:52:53 +0300

    icons

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import Cart from "../assets/icon/cart.svg?react";
+import Copy from "../assets/icon/copy.svg?react";
+import Copyright from "../assets/icon/copyright.svg?react";
+import Delete from "../assets/icon/delete.svg?react";
+import Edit from "../assets/icon/edit.svg?react";
+import Group from "../assets/icon/group.svg?react";
+import Icon from "../assets/icon/icon.svg?react";
+import Kids from "../assets/icon/kids.svg?react";
+import Logo from "../assets/icon/logo.svg?react";
+import Man from "../assets/icon/man.svg?react";
+import User from "../assets/icon/user.svg?react";
+import Vector from "../assets/icon/vector.svg?react";
+import Woman from "../assets/icon/woman.svg?react";
+
+export const Icons = {
+  Cart,
+  Copy,
+  Copyright,
+  Delete,
+  Edit,
+  Group,
+  Icon,
+  Kids,
+  Logo,
+  Man,
+  User,
+  Vector,
+  Woman,
+};
